e53c96337b620c673cf45dfaa0c83e4e93f07e04
Marcio Curvello <marciocurvello@gmail.com>
2022-06-19 20:02:36 -0300

    add controller

---
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseLibrary.API.Controllers
+{
+	public class AuthorsController : ControllerBase
+	{
+		private readonly ICourseLibraryRepository _courseLibraryRepository;
+
+		public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+		{
+			_courseLibraryRepository = courseLibraryRepository ??
+				throw new ArgumentNullException(nameof(courseLibraryRepository));
+		}
+
+		[HttpGet()]
+        public IActionResult GetAuthors()
+        {
+			var authorsFromRepo = _courseLibraryRepository.GetAuthors();
+			return Ok(authorsFromRepo);
+        }
+
+		public IActionResult GetAuthor(Guid authorId)
+        {
+			var authorFromRepo = _courseLibraryRepository.GetAuthor(authorId);
+
+			if (authorFromRepo == null)
+            {
+				return NotFound();
+            }
+
+			return Ok(authorFromRepo);
+        }
+    }
+}
+
